e1b534b40cd93eb2fd44fd6ae5333159b2d84ba1
Elham Parvizi Cherri <elhamparvizicherri@180.217.21.198.tigernet.wifi.dyn.clemson.edu>
2023-09-14 11:43:23 -0400

    json files

--- run-request.py
+++ run-request.py
@@ -1,7 +1,8 @@
 import requests
 import json
 import os
-
+import pandas
+import time 
 #check if it doesn't exist then make it
 if not os.path.exists("json_files"):
 	os.mkdir("json_files")
@@ -12,21 +13,28 @@ f =open("token", "r")
 token=f.read()
 f.close()
 
-github_session=requests.Secssion()
-githun_session.auth =("EParviziCherri","token")
+id_list = pandas.read_csv("seed.csv")
+id_list =id_list['ghid']
+
+github_session=requests.Session()
+github_session.auth =("EParviziCherri",token)
 
 response_text=requests.get(access_point+"/rate_limit").text
-
 print(json.loads(response_text))
 
-user_id= "erinata"
-response_text=github_session.get(access_point + "/users"+ user_id).text
+
+for user_id in id_list:
+
+	#user_id= "erinata"
+	print(user_id)
+	response_text=github_session.get(access_point + "/users"+ user_id).text
 
 
-json_text = json.loads(response_text)
+	json_text = json.loads(response_text)
 
 
-file_name = "json_files/" + user_id + ".json"
-f = open(file_name, "w")
-f.write(json.dumps(json_text))
-f.close()
+	file_name = "json_files/" + user_id + ".json"
+	f = open(file_name, "w")
+	f.write(json.dumps(json_text))
+	f.close()
+	time.sleep(1)
