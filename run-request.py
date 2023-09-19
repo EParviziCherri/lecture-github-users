5c7a4d3da36395efa94d882a7d9fe1187e923ca6
Elham Parvizi Cherri <elhamparvizicherri@122.216.21.198.tigernet.wifi.dyn.clemson.edu>
2023-09-19 11:08:54 -0400

    sleep

--- run-request.py
+++ run-request.py
@@ -25,16 +25,27 @@ print(json.loads(response_text))
 
 for user_id in id_list:
 
-	#user_id= "erinata"
-	print(user_id)
-	response_text=github_session.get(access_point + "/users"+ user_id).text
-
-
-	json_text = json.loads(response_text)
-
 
 	file_name = "json_files/" + user_id + ".json"
-	f = open(file_name, "w")
-	f.write(json.dumps(json_text))
-	f.close()
-	time.sleep(1)
+
+	if os.path.exists(file_name):
+
+
+		#pass
+		print("File exists: ",user_id)
+	else:
+
+		try:
+
+			print(user_id)
+			response_text=github_session.get(access_point + "/users"+ user_id).text
+			json_text=json.loads(response_text)
+			f = open(file_name+".tmp", "w")
+			f.write(json.dumps(json_text))
+			f.close()
+			os.rename(file_name+".tmp",file_name)
+		except Exception as e:
+			print(e)
+
+
+		time.sleep(5)
